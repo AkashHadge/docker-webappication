01eac1dbffc85fe511ca1889b4e2369c926a8323
AkashHadge <88318257+AkashHadge@users.noreply.github.com>
2021-09-21 19:52:07 +0530

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -15,7 +15,7 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
-    app.listen(8081, () => console.log('Listening on port 8081'));
+    app.listen(8081, () => console.log('Listening on port 8080'));
 }).catch((err) => {
     console.error(err);
     process.exit(1);
